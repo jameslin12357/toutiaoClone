b293c4d5e174e8f212d7880a33bd3a4f9fe7f5b3
jameslin12357 <jameslin12357@qq.com>
2019-09-07 08:42:09 +0800

    cm#3

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,16 +1,93 @@
+var form, layer, element;
+layui.use(['form', 'layedit', 'laydate', 'layer','element'], function () {
+    form = layui.form
+        , layer = layui.layer
+        , layedit = layui.layedit
+        , laydate = layui.laydate
+        , element = layui.element;
+});
+
 var active = true;
 $(window).scroll(function() {
     if (active){
         if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
             active = false;
             var offset = document.getElementsByClassName("card").length;
+            var topicid = "";
+            var pathname = window.location.pathname;
+            if (pathname === "/"){
+                topicid = 1;
+            } else {
+                if (pathname === "/videos/hot"){
+                    topicid = 2;
+                }
+                else if (pathname === "/videos/live"){
+                    topicid = 3;
+                }
+                else if (pathname === "/videos/tech"){
+                    topicid = 4;
+                }
+                else if (pathname === "/videos/ent"){
+                    topicid = 5;
+                }
+                else if (pathname === "/videos/games"){
+                    topicid = 6;
+                }
+                else if (pathname === "/videos/sports"){
+                    topicid = 7;
+                }
+                else if (pathname === "/videos/cars"){
+                    topicid = 8;
+                }
+                else if (pathname === "/videos/finance"){
+                    topicid = 9;
+                }
+                else if (pathname === "/videos/funny"){
+                    topicid = 10;
+                }
+            }
+
                $.ajax({
                    type: "get",
-                   url: `./videos?topicid=1&offset=${offset}`,
+                   url: `/videos?topicid=${topicid}&offset=${offset}`,
                    //url: `https://ditu.amap.com/detail/${poiId}/?src=mypage&callnative=0`,
                    dataType: "json",
                    success: function (data) {
-                       console.log(data);
+                       if (data.length === 0){
+                           layer.msg("没有数据");
+                       } else {
+                           var wrapperCard = document.getElementById('wrapperCard');
+                           var innerHTML = "";
+                           data.forEach(function(video){
+                                innerHTML += `     <div class="card">
+
+          <div class="card-body flex">
+            <div class="mr-15">
+              <video controls class="videoList" poster="/images/poster.png">
+
+                <source src="${video["videosrc"]}"
+                        type="video/mp4">
+              </video>
+            </div>
+            <div class="pt-20">
+              <a class="black" href="/videos/${video["videoid"]}"><h5 class="card-title">${video["title"]}</h5></a>
+
+
+              <div>
+                <a class="gray mr-5px" href="/users/${video["id"]}"><img class="profileList" src="${video["src"]}"></a>
+                <a class="gray mr-5px" href="/users/${video["id"]}">${video["username"]}</a>
+                                <span class="gray">${video["count"]}评论</span>
+
+                <span class="gray">${moment(video["created"]).startOf('hour').fromNow()}</span>
+              </div>
+            </div>
+
+          </div>
+        </div>`;
+                           });
+                           wrapperCard.innerHTML += innerHTML;
+                           active = true;
+                       }
                    },
                    error: function (item, err) {
                        console.log(err);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var redis = require("redis");
 const uuidv4 = require('uuid/v4');
 var mysql      = require('mysql');
+var moment = require('moment');
 
 function queryDB(sqlString,f){
   var connection = mysql.createConnection({
@@ -62,9 +63,7 @@ function sessionSetup(req, res, next){
   }
 }
 
-function formatDate(date) {
-  return moment(date).format('YYYY-MM-DD HH:mm:ss');
-}
+
 /* GET home page. */
 // if user is logged in or not get recommended videos
 // and paginate them and display on page
@@ -80,25 +79,121 @@ function formatDate(date) {
 // render popup message indicating end of data
 // else render data dynamically into html page
 router.get('/', sessionSetup, function(req, res, next) {
-  // console.log(typeof req.session.isAuthenticated);
-  // if (req.session.isAuthenticated === true){
-  //   console.log(req.session.isAuthenticated);
-  // }
-  queryDB("select v.id as videoid, v.title, v.src, v.created, u.id, u.username from videos v inner join users u on v.userid = u.id where v.topicid = '1' order by v.created desc limit 10;",
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '1' group by v.id order by v.created desc limit 10;",
       function(results){
-
-    console.log(results);
-    res.render('index', {
-      title: 'Home',
+    res.render('home/index', {
+      title: '首页',
       req: req,
-      results: results
+      results: results,
+      moment: moment
     });});
-
 });
 
-router.get('/videos', function(req, res, next){
+router.get('/videos/hot', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '2' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/hot', {
+          title: '热点',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
 
-})
+router.get('/videos/live', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '3' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/live', {
+          title: '直播',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos/tech', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '4' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/tech', {
+          title: '科技',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos/ent', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '5' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/ent', {
+          title: '娱乐',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos/games', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '6' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/games', {
+          title: '游戏',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos/sports', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '7' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/sports', {
+          title: '体育',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos/cars', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '8' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/cars', {
+          title: '汽车',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos/finance', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '9' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/finance', {
+          title: '财经',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos/funny', sessionSetup, function(req, res, next) {
+  queryDB("select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = '10' group by v.id order by v.created desc limit 10;",
+      function(results){
+        res.render('home/funny', {
+          title: '搞笑',
+          req: req,
+          results: results,
+          moment: moment
+        });});
+});
+
+router.get('/videos', function(req, res, next) {
+  queryDB(`select v.id as videoid, v.title, v.src as videosrc, v.created, u.id, u.username, u.src, count(body) as count from videos v inner join users u on v.userid = u.id left join comments c on v.id = c.videoid where v.topicid = ${req.query.topicid} group by v.id order by v.created desc limit 10 offset ${req.query.offset};`,
+      function (results) {
+        res.json(results);
+      });
+});
 
 
 module.exports = router;
